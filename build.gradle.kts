040626bb199cc9bbfc90ccd8771f7e266dde83fb
Apollointhehouse <apollocameronboot@gmail.com>
2025-08-01 21:18:36 +1200

    libertas!

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlinxSerialization)
 }
 
-group = "me.user"
+group = "me.apollointhehouse"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,19 +11,15 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    val nativeTarget = mingwX64("native")
 
     nativeTarget.apply {
+        compilations.getByName("main") {
+            cinterops {
+                val windows by creating
+            }
+        }
+
         binaries {
             executable {
                 entryPoint = "main"
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,21 +1,45 @@
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+@file:OptIn(ExperimentalForeignApi::class)
 
-@Serializable
-private data class Message(
-    val topic: String,
-    val content: String,
-)
-
-private val PrettyPrintJson = Json {
-    prettyPrint = true
-}
+import kotlinx.cinterop.*
+import platform.windows.*
 
 fun main() {
-    val message = Message(
-        topic = "Kotlin/Native",
-        content = "Hello!"
-    )
-    println(PrettyPrintJson.encodeToString(message))
+    val procName = "veyon-worker.exe"
+    val procID = getProcessID(procName)
+
+    if (procID == 0u) {
+        error("id not found for process with name: $procName")
+    }
+
+    println("Killing $procName with pid: $procID")
+
+    val permsFlag: DWORD = (PROCESS_TERMINATE).toUInt()
+
+    val procHandle: HANDLE? = OpenProcess(permsFlag, 0, procID)
+    TerminateProcess(procHandle, 1u)
+    CloseHandle(procHandle)
 }
+
+private fun getProcessID(processName: String): UInt = memScoped {
+    val snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS.toUInt(),0U)
+    val processEntry = alloc<PROCESSENTRY32W>().apply {
+        dwSize = sizeOf<PROCESSENTRY32W>().convert()
+    }
+
+    if (Process32FirstW(snapshot,processEntry.ptr) == 0) {
+        CloseHandle(snapshot)
+        return 0U
+    }
+
+    do {
+        val name = processEntry.szExeFile.toKString()
+        if (processName == name) {
+            CloseHandle(snapshot)
+            return processEntry.th32ProcessID
+        }
+    } while (Process32NextW(snapshot, processEntry.ptr) != 0)
+
+    CloseHandle(snapshot)
+    return 0U
+}
+
